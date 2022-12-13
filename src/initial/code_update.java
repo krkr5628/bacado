1225713accb09c8b09c4509dc51720d69e6beb18
krkr5628 <krkr5628@gmail.com>
2022-12-13 16:15:35 +0900

    dart_code와 krx_short_code 매칭이 불가능한 목록은 left.csv로 저장
    DART_CODE는 신규로 계속 업데이트

--- src/initial/code_update.java
+++ src/initial/code_update.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class code_update {
     private static final String save_route_for_kospi_integration = "D:\\Drive\\Code\\bacado\\csv\\list\\kospi_integration.csv";
     private static final String save_route_for_kosdak_integration = "D:\\Drive\\Code\\bacado\\csv\\list\\kosdak_integration.csv";
+    private static final String save_route_for_kospi_left = "D:\\Drive\\Code\\bacado\\csv\\list\\kospi_left.csv";
+    private static final String save_route_for_kosdak_left = "D:\\Drive\\Code\\bacado\\csv\\list\\kosdak_left.csv";
     private static final String save_route_for_kospi_status = "D:\\Drive\\Code\\bacado\\csv\\list\\kospi_status.csv";
     private static final String save_route_for_kodak_status = "D:\\Drive\\Code\\bacado\\csv\\list\\kosdak_status.csv";
     private static final String save_route_for_kospi_update_list = "D:\\Drive\\Code\\bacado\\csv\\list\\kospi_update_list.csv";
@@ -24,27 +26,34 @@ public class code_update {
         //
         HashMap<String, String> dart_code = ListToHashMap(dart_code_update.dart_list);
         //
-        update_status.Update_status(integration(short_code_update.short_list.get(0), dart_code, save_route_for_kospi_integration),
-                "kospi", save_route_for_kospi_status,save_route_for_kospi_update_list);
-        update_status.Update_status(integration(short_code_update.short_list.get(1), dart_code, save_route_for_kosdak_integration),
-                "kosdak", save_route_for_kodak_status,save_route_for_kodak_update_list);
+        update_status.Update_status(integration(short_code_update.short_list.get(0), dart_code,
+                        save_route_for_kospi_integration, save_route_for_kospi_left),
+                "kospi", save_route_for_kospi_status, save_route_for_kospi_update_list);
+        update_status.Update_status(integration(short_code_update.short_list.get(1), dart_code,
+                        save_route_for_kosdak_integration, save_route_for_kosdak_left),
+                "kosdak", save_route_for_kodak_status, save_route_for_kodak_update_list);
     }
     private static HashMap<String, String> ListToHashMap(List<List<String>> input_list){
         HashMap<String, String> tmp = new HashMap<>();
         for(List<String> line : input_list){
-            tmp.put(line.get(0), line.get(1));
+            tmp.put(line.get(1), line.get(0)); // corp_name, corp_cord
         }
         return tmp;
     }
-    private static List<List<String>> integration(List<List<String>> first, HashMap<String, String> second, String save_route){
-        List<List<String>> save_file = new ArrayList<>();
-        for(List<String> tmp : first){
-            if(second.containsKey(tmp.get(0))){
-                save_file.add(List.of(tmp.get(0),second.get(tmp.get(0)),tmp.get(1),tmp.get(2)));
+    private static List<List<String>> integration(List<List<String>> short_code, HashMap<String, String> dart_code, String save_route_for_integration, String save_route_for_left){
+        List<List<String>> integration_save_file = new ArrayList<>();
+        List<List<String>> integration_left_file = new ArrayList<>();
+        for(List<String> tmp : short_code){
+            if(dart_code.containsKey(tmp.get(2))){
+                integration_save_file.add(List.of(tmp.get(0), dart_code.get(tmp.get(2)), tmp.get(2)));
+            }
+            else{
+                integration_left_file.add(List.of(tmp.get(0),tmp.get(1)));
             }
         }
-        CSV.writeCSV(save_route, save_file);
-        return save_file;
+        CSV.writeCSV(save_route_for_integration, integration_save_file);
+        CSV.writeCSV(save_route_for_left, integration_left_file);
+        return integration_save_file;
     }
 
 }
