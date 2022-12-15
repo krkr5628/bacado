b33adbf69d57f885afc6d829a096f8728fa12680
krkr5628 <krkr5628@gmail.com>
2022-12-15 21:15:43 +0900

    initial package로 이동

--- src/initial/ifrs_update.java
+++ src/initial/ifrs_update.java
@@ -1,7 +1,8 @@
-package export;
+package initial;
+
+import static initial.setting.dart_api_key;
 
 import load_save.CSV;
-import static initial.setting.dart_api_key;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,7 +17,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ifrs_financial {
+public class ifrs_update {
     private static int cnt = 0;
     private static final String write_route_for_dart_code = "D:\\Drive\\Code\\bacado\\csv\\";
     public static void download_financial(String dart_code, String year, String half, String fs, String market) throws IOException, ParseException {
@@ -51,28 +52,3 @@ public class ifrs_financial {
         System.out.println(cnt++);
     }
 }
-/*
-        public static HashSet<String> jong_bok = new HashSet<>();
-        jong_bok.add(((JSONObject)value1.get(i)).get("sj_nm") + "," + ((JSONObject)value1.get(i)).get("account_id") + "," + ((JSONObject)value1.get(i)).get("account_nm") + "," + ((JSONObject)value1.get(i)).get("account_detail"));
-        //
-        String url_plus2 = "http://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=esy%2BILqhjceVIiQy96oHrDjiEKhM9TVTNUIovRo954WK32I1pCD8QkU6PVbIRrxa5R9mUYJo9y7fiKde77op5w%3D%3D&resultType=json&likeSrtnCd="+short_code;
-        URL url2 = new URL(url_plus2);
-        BufferedReader bf2;
-        bf2 = new BufferedReader(new InputStreamReader(url2.openStream(), StandardCharsets.UTF_8));
-        //
-        String response2 = bf2.readLine();
-        //
-        JSONObject myjson2 = (JSONObject)jsonParser.parse(response2);
-        //
-        JSONObject response = (JSONObject)myjson2.get("response");
-        JSONObject body = (JSONObject)response.get("body");
-        JSONObject items = (JSONObject)body.get("items");
-        JSONArray item = (JSONArray)items.get("item");
-        JSONObject value2 = (JSONObject)item.get(0);
-        //
-        arr.add(value2.get("basDt")); // 시간 basDt
-        arr.add(value2.get("clpr")); // 종가 clpr
-        arr.add(value2.get("trqu")); // 거래량 trqu
-        arr.add(value2.get("mrktTotAmt")); // 시가총액 mrktTotAmt
-        arr.add(value2.get("lstgStCnt")); // 상장좌수 lstgStCnt
-        */
