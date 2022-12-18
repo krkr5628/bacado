e64db0b25cb471af98d9f8d62572ae296eef4c31
krkr5628 <krkr5628@gmail.com>
2022-12-18 14:43:34 +0900

    Timer추가 향후 하루에 한번만 작동하도록 수정 향후 업데이트 실패한 것에 대해서만 시도하도록 수정

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
     private static final String read_route_for_kospi_update_list = "D:\\Drive\\Code\\bacado\\csv\\list\\kospi_update_list.csv";
@@ -14,8 +16,15 @@ public class Main {
     private static final String[] half = {"11013", "11012", "11014", "11011"};
 
     public static void main(String[] args) throws IOException, ParseException, ParserConfigurationException, SAXException {
-        //initial_update.Code_update();
-        ifrs_update.download_financial(CSV.readCSV(read_route_for_kospi_update_list), "kospi");
-        //ifrs_update.download_financial(CSV.readCSV(read_route_for_kosdak_update_list), "kosdak");
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                //code_standarzation.Code_standarzation();
+                //initial_update.Code_update();
+                //ifrs_update.download_financial(CSV.readCSV(read_route_for_kospi_update_list), "kospi");
+                //ifrs_update.download_financial(CSV.readCSV(read_route_for_kosdak_update_list), "kosdak");
+            }
+        };
+        new Timer().scheduleAtFixedRate(task, 1000, 300000);
     }
 }
