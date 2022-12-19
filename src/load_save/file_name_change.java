fee7356891f040575bfc85ae8d6853160c210f88
krkr5628 <krkr5628@gmail.com>
2022-12-19 13:29:49 +0900

    class error

--- src/load_save/file_name_change.java
+++ src/load_save/file_name_change.java
@@ -15,20 +15,21 @@ public class file_name_change {
     private static final String save_route_for_kosdak_integration = "D:\\Drive\\Code\\bacado\\csv\\list\\kosdak_integration.csv";
     private static final String write_route_for_dart_code = "D:\\Drive\\Code\\bacado\\csv\\";
     public static void File_name_change(){
-        List<List<String>> stock_code = CSV.readCSV(save_route_for_kosdak_integration);
+        List<List<String>> stock_code = CSV.readCSV(save_route_for_kospi_integration);
         for(int i = 0; i < 200; i++){
             for(int j = 2012; j <= 2022; j++) {
                 String year_w = Integer.toString(j);
                 for (int k = 0; k < 4; k++) {
                     for(int t = 0; t <= 1; t++){
-                        Path tmp = Paths.get(write_route_for_dart_code + "kosdak" + "\\" + stock_code.get(i).get(1) + "\\"
-                                + stock_code.get(i).get(1) + "_" + year_w + "_" + half[k] + "_" + fs[t]);
-                        Path tmp2 = Paths.get(write_route_for_dart_code + "kosdak" + "\\" + stock_code.get(i).get(1) + "\\"
-                                + stock_code.get(i).get(1) + "_" + year_w + "_" + half[k] + "_" + fs[t] + ".csv");
+                        Path tmp = Paths.get(write_route_for_dart_code + "kospi" + "\\" + stock_code.get(i).get(1) + "\\"
+                                + stock_code.get(i).get(1) + "_financial_history.csv");
+                        Path tmp2 = Paths.get(write_route_for_dart_code + "kospi" + "\\" + stock_code.get(i).get(1) + "\\"
+                                + stock_code.get(i).get(1) + "_financial_company.csv");
+
                         if(Files.exists(tmp)){
-                            File kospi_file = new File(tmp.toUri());
-                            File kospi_newfile = new File(tmp2.toUri());
-                            boolean result = kospi_file.renameTo(kospi_newfile);
+                            File file = new File(tmp.toUri());
+                            File new_file = new File(tmp2.toUri());
+                            boolean result = file.renameTo(new_file);
                             System.out.println(result);
                         }
                         if(j == end_year && k == end_half) break;
@@ -36,14 +37,5 @@ public class file_name_change {
                 }
             }
         }
-    }
-    private static String code_length(String tmp){
-        String new_code = tmp;
-        if(tmp.length() < 8){
-            for(var i = 0; i < (8 - tmp.length()); i++){
-                new_code = "0" + new_code;
-            }
-        }
-        return new_code;
     }
 }
