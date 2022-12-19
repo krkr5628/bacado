37b796306c3f6c0b5621047217067e6b2f22cbb1
krkr5628 <krkr5628@gmail.com>
2022-12-19 16:18:00 +0900

    년도 문제 수정

--- src/initial/code/update_status.java
+++ src/initial/code/update_status.java
@@ -15,28 +15,28 @@ public class update_status {
     private static final String[] half = {"11013", "11012", "11014","11011"};
     private static final String route_for_dart_code = "D:\\Drive\\Code\\bacado\\csv\\";
     public static void Update_status(List<List<String>> code_list, String market, String save, String save2){
-        List<List<String>> status_tmp = update(code_list, market, Integer.toString(end_year));
+        List<List<String>> status_tmp = update(code_list, market);
         CSV.writeCSV(save, status_tmp);
         Update_list(save2, status_tmp);
     }
-    private static List<List<String>> update(List<List<String>> code, String market, String year){
+    private static List<List<String>> update(List<List<String>> code, String market){
         List<List<String>> save = new ArrayList<>();
         for (List<String> line : code) {
-            save.add(check(line, market, year));
+            save.add(check(line, market));
         }
         return save;
     }
-    private static List<String> check(List<String> line, String market, String year){
+    private static List<String> check(List<String> line, String market){
         if(Files.exists(Path.of(route_for_dart_code + market + "\\" + line.get(1) + "\\"
                 + line.get(1) + "_financial_company.csv"))) {
-            return List.of(line.get(1), line.get(2), year + "-" + half[end_half], "financial_company");
+            return List.of(line.get(1), line.get(2), "2011-11011", "financial_company");
         }
         String update_line = "2011-11011";
         for(int j = 2012; j <= 2022; j++) {
             for (int k = 0; k < 4; k++) {
                 if(Files.exists(Path.of(route_for_dart_code + market + "\\" + line.get(1) + "\\"
-                        + line.get(1) + "_" + year + "_" + half[end_half] + "_" + "OFS" + ".csv"))){
-                    update_line = year + "_" + half[end_half];
+                        + line.get(1) + "_" + j + "_" + half[end_half] + "_" + "OFS" + ".csv"))){
+                    update_line = j + "-" + half[end_half];
                 }
             }
         }
