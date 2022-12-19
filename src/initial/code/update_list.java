d06161e09294b7fe840f6b54a4690dc374770d84
krkr5628 <krkr5628@gmail.com>
2022-12-19 16:22:49 +0900

    ! 수정

--- src/initial/code/update_list.java
+++ src/initial/code/update_list.java
@@ -10,7 +10,7 @@ public class update_list {
     public static void Update_list(String route, List<List<String>> update_list){
         List<List<String>> save_file = new ArrayList<>();
         for(List<String> tmp : update_list){
-            if(!tmp.get(3).equals("update")){
+            if(tmp.get(3).equals("update")){
                 save_file.add(tmp);
             }
         }
