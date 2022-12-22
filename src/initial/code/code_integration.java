4c5b124a2f5d97972fcbe0d949bae4e3df91cd08
krkr5628 <krkr5628@gmail.com>
2022-12-22 15:01:02 +0900

    dart_code를 list로 받고 내부에서 hashmap으로 변환 매칭 가능한 기존 코드 = integration 메칭 가능한 새 코드 =  integration, integration_new 매칭 불가능한 이름 다른 기존 코드 = integration_new 매칭 불가능한 우선주 = integration_left *새코드는 비로 디렉토리 생성하도록 수정.

--- src/initial/code/code_integration.java
+++ src/initial/code/code_integration.java
@@ -1,26 +1,57 @@
 package initial.code;
 
 import load_save.CSV;
+import load_save.ListToHashMap;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static initial.setting.standard_date;
+
 public class code_integration {
-    public static void Code_integration(List<List<String>> short_code, HashMap<String, String> dart_code, String save_route_for_integration, String save_route_for_left){
-        List<List<String>> integration_save_file = new ArrayList<>();
-        List<List<String>> integration_left_file = new ArrayList<>();
-        // krx 이름과 dart 호환명이 일치하면 integration으로 이동
-        // krx 이름이 바뀌거나 새로 추가된 코드중 dart 호환명과 일치하지 안을 경우 left로 이동
+    public static void Code_integration(List<List<String>> dart_code, List<List<String>> short_code, String market,
+                                        String save_route_for_integration, String save_route_for_new, String save_route_for_left) throws IOException {
+        List<List<String>> integration = new ArrayList<>(); // dart_code와 호환되는 krx_code
+        List<List<String>> integration_left = new ArrayList<>(); // dart_code와 호환불가한 krx_code
+        List<List<String>> integration_new = new ArrayList<>(); // dart_code와 호환되는 새로운 krx_code
+        integration_new.add(List.of(standard_date));
+        //
+        HashMap<String, String> dart_code_hashmap = ListToHashMap.listTohashMap(dart_code, 1, 0); // dart_code hash_map
+        HashMap<String, String> old_integration_code_hashmap = ListToHashMap.listTohashMap(CSV.readCSV(save_route_for_integration), 0, 2);
         for(List<String> tmp : short_code){
-            if(dart_code.containsKey(tmp.get(2))){
-                integration_save_file.add(List.of(tmp.get(0), dart_code.get(tmp.get(2)), tmp.get(2)));
+            if(dart_code_hashmap.containsKey(tmp.get(2))){
+                // 매칭된 모든 정목 추가
+                integration.add(List.of(tmp.get(0), dart_code_hashmap.get(tmp.get(2)), tmp.get(2)));
+                // 매칭된 것 중에서 신규 종목
+                if(!old_integration_code_hashmap.containsKey(tmp.get(0))){
+                    integration_new.add(List.of(tmp.get(0),tmp.get(1),"NEW"));
+                    // 신규 종목 디렉토리 생성
+                    String path = "D:\\Drive\\Code\\bacado\\csv\\" + market + "\\" + dart_code_hashmap.get(tmp.get(2));
+                    if (!Files.exists(Path.of(path))) {
+                        Files.createDirectories(Paths.get(path));
+                    }
+                }
             }
             else{
-                integration_left_file.add(List.of(tmp.get(0),tmp.get(1)));
+                //매칭 불가능한 것
+                //코드가 존재하면서 이름이 다른것
+                if(old_integration_code_hashmap.containsKey(tmp.get(0))){
+                    integration_new.add(List.of(tmp.get(0),tmp.get(1),"NAME"));
+                }
+                else{
+                    //코드가 존재하지 않으면서 이름도 다른것
+                    integration_left.add(List.of(tmp.get(0),tmp.get(1)));
+                }
             }
         }
-        CSV.writeCSV(save_route_for_integration, integration_save_file);
-        CSV.writeCSV(save_route_for_left, integration_left_file);
+        CSV.writeCSV(save_route_for_integration, integration);
+        CSV.writeCSVoverride(save_route_for_new, integration_new);
+        CSV.writeCSV(save_route_for_left, integration_left);
     }
+
 }
