4b18c6c59c1de604772e3d7841bd1473b5558e68
krkr5628 <krkr5628@gmail.com>
2023-01-21 17:26:46 +0900

    20230121 웹사이트 개발로 잠시 중단

--- src/initial/check_update.java
+++ src/initial/check_update.java
@@ -4,7 +4,7 @@ import load_save.CSV;
 
 import java.util.List;
 
-public class check_update {
+public class  check_update {
 
     public static boolean Check_update(String route){
         List<List<String>> update_log = CSV.readCSV(route);
